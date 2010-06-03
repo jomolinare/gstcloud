cebfd56eeb6880b4ce9094c412c4bb6ddb7eb461
prabuddha.ghosh <prabuddha.ghosh@99842a6d-93fc-5f8c-54a6-f6a797d3850d>
2010-06-03 13:45:06 +0000

    This is a District class which constitutes the variables for storing the details of the districts according to the state's Id

--- GSTCloud/src/com/rmsi/lim/gstcloud/shared/Districts.java
+++ GSTCloud/src/com/rmsi/lim/gstcloud/shared/Districts.java
@@ -1,5 +1,87 @@
 package com.rmsi.lim.gstcloud.shared;
 
-public class Districts {
+import javax.jdo.annotations.ForeignKey;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable = "false")
+public class Districts implements IsSerializable
+{
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Long districtId;
+	
+	@ForeignKey
+	@Persistent
+	private Long state_Id;
+	
+    @Persistent
+    private String districtName;
+
+    @Persistent
+    private Double latitude;
+    
+    @Persistent
+    private Double longitude;
+
+    public Districts()
+    {
+    	
+    }
+    
+    public Districts(Long state_Id,String districtName,Double latitude, Double longitude) 
+    {
+        this.state_Id = state_Id;
+        this.districtName = districtName;
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+
+    public Long getDistrictId() 
+    {
+        return districtId;
+    }
+    
+    public long getState_Id() 
+    {
+        return state_Id;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+    
+    public void setDistrictId(Long districtId) 
+    {
+    	this.districtId = districtId;
+    }
+    
+    public void setState_Id(Long state_Id) 
+    {
+    	this.state_Id = state_Id;
+    }
+
+    public void setDistrictName(String stateName) {
+        this.districtName = stateName;
+    }
+    
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
