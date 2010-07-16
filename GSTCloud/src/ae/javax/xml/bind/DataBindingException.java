7e7eb38d347a4f9c24d5edf4c3850991bc7b2f5d
vish0590 <vish0590@gmail.com>
2010-07-16 12:46:01 +0000

    Adding JAXB modified for compatibility with Appengine4

---
+++ GSTCloud/src/ae/javax/xml/bind/DataBindingException.java
@@ -0,0 +1,24 @@
+package ae.javax.xml.bind;
+
+import ae.javax.xml.bind.JAXBException;
+
+/**
+ * Exception that represents a failure in a JAXB operation.
+ *
+ * <p>
+ * This exception differs from {@link JAXBException} in that
+ * this is an unchecked exception, while <tt>JAXBException</tt>
+ * is a checked exception.
+ *
+ * @see JAXB
+ * @since JAXB2.1
+ */
+public class DataBindingException extends RuntimeException {
+    public DataBindingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataBindingException(Throwable cause) {
+        super(cause);
+    }
+}
