10c92602a86f5ed16c44210cb72991c0fd40943b
vish0590@gmail.com <vish0590@gmail.com@99842a6d-93fc-5f8c-54a6-f6a797d3850d>
2010-07-16 12:38:10 +0000

    Adding JAXB modified for compatibility with Appengine2

---
+++ trunk/GSTCloud/src/ae/com/sun/xml/bind/v2/model/annotation/XmlValueQuick.java
@@ -0,0 +1,32 @@
+
+package ae.com.sun.xml.bind.v2.model.annotation;
+
+import java.lang.annotation.Annotation;
+
+import ae.javax.xml.bind.annotation.XmlValue;
+
+final class XmlValueQuick
+    extends Quick
+    implements XmlValue
+{
+
+    private final XmlValue core;
+
+    public XmlValueQuick(Locatable upstream, XmlValue core) {
+        super(upstream);
+        this.core = core;
+    }
+
+    protected Annotation getAnnotation() {
+        return core;
+    }
+
+    protected Quick newInstance(Locatable upstream, Annotation core) {
+        return new XmlValueQuick(upstream, ((XmlValue) core));
+    }
+
+    public Class<XmlValue> annotationType() {
+        return XmlValue.class;
+    }
+
+}
---
+++ trunk/GSTCloud/src/ae/com/sun/xml/bind/v2/schemagen/xmlschema/ExplicitGroup.java
@@ -0,0 +1,11 @@
+
+package ae.com.sun.xml.bind.v2.schemagen.xmlschema;
+
+import com.sun.xml.txw2.TypedXmlWriter;
+
+public interface ExplicitGroup
+    extends Annotated, NestedParticle, Occurs, TypedXmlWriter
+{
+
+
+}
