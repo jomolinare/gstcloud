9e92527c7002e0a4135fd763be68909347276961
Prabuddha.Ghosh <Prabuddha.Ghosh@99842a6d-93fc-5f8c-54a6-f6a797d3850d>
2010-07-21 12:00:06 +0000

    Adding new model classes for Tower

---
+++ GSTCloud/src/com/rmsi/lim/gstcloud/client/model/TowerDTO.java
@@ -0,0 +1,102 @@
+package com.rmsi.lim.gstcloud.client.model;
+
+import java.util.List;
+
+
+import com.google.gwt.user.client.rpc.IsSerializable; 
+
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import javax.jdo.annotations.IdentityType;
+
+//@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable = "false")
+//@Inheritance(customStrategy = "complete-table")
+public class TowerDTO  implements IsSerializable{
+//public class LandmarkDTO  extends Geohashed implements IsSerializable{
+//    @PrimaryKey
+//    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+//    private Long id;
+
+//    @Persistent
+	private boolean currentlySelected;
+	
+    private String category;
+
+//    @Persistent
+    private Double latitude;
+    
+//    @Persistent
+    private Double longitude;
+    
+//    @Persistent
+    private String placeName;
+
+    public TowerDTO()
+    {
+    	
+    }
+    
+    public TowerDTO(String category,Double latitude, Double longitude, String placeName,boolean selected) {
+        this.category = category;
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.placeName = placeName;
+        this.currentlySelected=selected;
+//        this.setGeoCells(geoCells);
+    }
+
+//    public long getid() {
+//        return id;
+//    }
+
+//    public LandmarkDTO(Landmark landmark) {
+//    	this.setCategory(landmark.getPlaceName());
+//		this.setLatitude(landmark.getLatitude());
+//		this.setLongitude(landmark.getLongitude());
+//		 this.setPlaceName(landmark.getPlaceName());
+//	}
+
+	public Boolean getCurrentlySelected(){
+		return currentlySelected;
+	}
+    
+    public String getCategory() {
+        return category;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+    
+    public String getPlaceName() {
+        return placeName;
+    }
+    
+    public void setCurrentlySelected(boolean select){
+    	this.currentlySelected=select;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPlaceName(String placsName) {
+        this.placeName = placsName;
+    }
+    
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ GSTCloud/src/com/rmsi/lim/gstcloud/server/model/Tower.java
@@ -0,0 +1,109 @@
+package com.rmsi.lim.gstcloud.server.model;
+
+import java.util.List;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+
+import com.beoui.geocell.GeocellManager;
+import com.beoui.geocell.model.LocationCapable;
+import com.beoui.geocell.model.Point;
+import com.google.gwt.user.client.rpc.IsSerializable;
+import com.rmsi.lim.gstcloud.client.model.TowerDTO;
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable = "false")
+@Inheritance(customStrategy = "complete-table")
+public class Tower extends Geohashed implements LocationCapable,IsSerializable
+{
+  @Persistent
+    private String category;
+
+  @Persistent
+    private String owner;
+  
+  //Stores the coverage of a tower in wkt form
+  @Persistent String coverage;
+  
+  //Stores the height of a tower in meters
+  @Persistent
+  private Double height;
+  
+  @Persistent
+    private Double latitude;
+    
+    @Persistent
+    private Double longitude;
+    
+    @Persistent
+    private String placeName;
+
+    
+    
+    public Tower(String category,Double latitude, Double longitude, String placeName,List<String> geoCells) {
+        this.category = category;
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.placeName = placeName;
+        this.setGeoCells(geoCells);
+    }
+
+//    public long getid() {
+//        return id;
+//    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+    
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+    
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+	public Tower(TowerDTO station) {
+		this.setCategory(station.getCategory());
+		this.setLatitude(station.getLatitude());
+		this.setLongitude(station.getLongitude());
+		 this.setPlaceName(station.getPlaceName());
+	     this.setGeoCells(GeocellManager.generateGeoCell(new com.beoui.geocell.model.Point(station.getLatitude(),station.getLongitude())));
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<String> getGeocells() {
+		
+		return this.getGeoCells();
+	}
+
+	public String getKeyString() {
+        return Long.valueOf(this.getObjectId()).toString();
+    }
+
+    public Point getLocation() {
+        return new Point(this.getLatitude(),this.getLongitude());
+    }
+
+}
