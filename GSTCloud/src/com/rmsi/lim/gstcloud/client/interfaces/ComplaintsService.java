b315099e4637a580db4205767a6029d535076b12
prabuddha.ghosh <prabuddha.ghosh@99842a6d-93fc-5f8c-54a6-f6a797d3850d>
2011-05-20 11:07:17 +0000

    Please ignore the previous two revisions

---
+++ GSTCloud/src/com/rmsi/lim/gstcloud/client/interfaces/ComplaintsService.java
@@ -0,0 +1,14 @@
+package com.rmsi.lim.gstcloud.client.interfaces;
+
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.rmsi.lim.gstcloud.client.model.ComplaintDTO;
+
+@RemoteServiceRelativePath("complaint")
+public interface ComplaintsService extends RemoteService{
+
+	String loadStation(ComplaintDTO complaint);
+	List<ComplaintDTO> getOutages();
+}
